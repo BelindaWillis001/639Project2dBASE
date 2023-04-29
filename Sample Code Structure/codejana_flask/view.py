5c31d460ea7d745c2c48af42f3113d2df3feee46
hidodai <hido.dai@lincolnuni.ac.nz>
2023-04-30 10:45:43 +1200

    Add an page from web to learn calendar

--- Sample Code Structure/codejana_flask/view.py
+++ Sample Code Structure/codejana_flask/view.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/user')
 def user():
     return "user"
+
+@app.route('/leavecalender')
+def leavecalender():
+    return render_template('leavecalender.html')
